a5ed79a8fe0ee525fab4881f800abe516175d13c
PraneethJain <praneethjain005@gmail.com>
2022-12-01 10:57:52 +0530

    Solved Day 1

---
+++ Day 1/main.jl
@@ -0,0 +1,17 @@
+open("./Day 1/input.txt") do f
+    L = []
+    cur_sum = 0
+    for line in readlines(f)
+        if line == ""
+            push!(L, cur_sum)
+            cur_sum = 0
+        else
+            cur_sum += parse(Int64, line)
+        end
+    end
+
+    sorted_L = sort(L, rev = true)
+
+    println("Highest is $(sorted_L[1])")
+    println("Sum of 3 highest is $(sorted_L[1] + sorted_L[2] + sorted_L[3])")
+end
