e78af347676c6571dfc3817da02eba152be7dee2
PraneethJain <praneethjain005@gmail.com>
2022-12-02 10:59:43 +0530

    Solved Day

---
+++ Day 2/Part1.py
@@ -0,0 +1,22 @@
+result = 0
+mapping = {"X": 1, "Y": 2, "Z": 3}
+with open("input.txt", "r") as f:
+    for line in f.readlines():
+        p1, p2 = line.split()
+        result += mapping[p2]
+        if (
+            (p1 == "A" and p2 == "X")
+            or (p1 == "B" and p2 == "Y")
+            or (p1 == "C" and p2 == "Z")
+        ):
+            result += 3
+        elif (
+            (p1 == "A" and p2 == "Y")
+            or (p1 == "B" and p2 == "Z")
+            or (p1 == "C" and p2 == "X")
+        ):
+            result += 6
+        else:
+            result += 0
+
+print(result)
---
+++ Day 2/Part2.py
@@ -0,0 +1,27 @@
+result = 0
+mapping = {"A": 1, "B": 2, "C": 3}
+
+with open("input.txt", "r") as f:
+    for line in f.readlines():
+        p1, p2 = line.split()
+        if p2 == "Z":
+            result += 6
+            if p1 == "A":
+                result += 2
+            elif p1 == "B":
+                result += 3
+            elif p1 == "C":
+                result += 1
+        elif p2 == "Y":
+            result += 3
+            result += mapping[p1]
+        else:
+            if p1 == "A":
+                result += 3
+            elif p1 == "B":
+                result += 1
+            else:
+                result += 2
+
+
+print(result)
