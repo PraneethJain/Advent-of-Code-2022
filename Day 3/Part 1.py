6681a847a85dec712b00ed5435cfc6ce25f79a0a
PraneethJain <praneethjain005@gmail.com>
2022-12-03 11:03:51 +0530

    Solved Day 3

---
+++ Day 3/Part 1.py
@@ -0,0 +1,12 @@
+mapping = {chr(i + 96): i for i in range(1, 27)} | {
+    chr(i + 64): i + 26 for i in range(1, 27)
+}
+
+with open("./Day 3/input.txt", "r") as f:
+    result = 0
+    for line in f.readlines():
+        mid = len(line) // 2
+        left, right = set(line[:mid]), set(line[mid:])
+        result += mapping[next(iter(left.intersection(right)))]
+
+    print(result)
---
+++ Day 3/Part 2.py
@@ -0,0 +1,18 @@
+mapping = {chr(i + 96): i for i in range(1, 27)} | {
+    chr(i + 64): i + 26 for i in range(1, 27)
+}
+
+with open("./Day 3/input.txt", "r") as f:
+    result = 0
+    lines = f.readlines()
+    length = len(lines)
+    for i in range(0, length, 3):
+        a = (
+            set(lines[i])
+            .intersection(set(lines[i + 1]))
+            .intersection(set(lines[i + 2]))
+        )
+        a.remove("\n")
+        result += mapping[next(iter(a))]
+
+    print(result)
