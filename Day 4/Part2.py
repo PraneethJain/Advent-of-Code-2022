078b718fb75cb33720e93275656dea2153958518
PraneethJain <praneethjain005@gmail.com>
2022-12-04 10:54:49 +0530

    Solved Day 4

---
+++ Day 4/Part2.py
@@ -0,0 +1,11 @@
+with open("input.txt") as f:
+    result = 0
+    for line in f.readlines():
+        L1, L2 = [list(map(int, a.split("-"))) for a in line.split(",")]
+        A1 = {i for i in range(L1[0], L1[1] + 1)}
+        A2 = {i for i in range(L2[0], L2[1] + 1)}
+        inter = A1.intersection(A2)
+        if inter:
+            result += 1
+
+    print(result)
